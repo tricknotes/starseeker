5c40ccfdf1e98d8211a4c6f4681e79878bd75fc3
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-21 00:14:34 +0900

    Remove unnecessary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-
   attr_accessible :email, :password, :password_confirmation, :authentications_attributes
-  authenticates_with_sorcery! do |config|
-    config.authentications_class = Authentication
-  end
 
   has_many :authentications, :dependent => :destroy
   accepts_nested_attributes_for :authentications
