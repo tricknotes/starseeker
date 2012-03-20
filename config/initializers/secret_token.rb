ae15a10d98c51c1bace9715a5421fd753d7d3ebc
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-20 18:12:42 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WatchMen::Application.config.secret_token = 'e86de9e142d99b7dc1cc4a0398146d90db9444d56625ac85a98ebf08422e9cd91eee36ffdd98fe3732fa1365447c4416727c89ea6f2b1f9708fe3503f3cc84f7'
