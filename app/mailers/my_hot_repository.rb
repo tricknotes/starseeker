3bf6f71940fecd37d2139cc32671f8c0809bc556
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-24 15:09:08 +0900

    Deliver mail

---
+++ app/mailers/my_hot_repository.rb
@@ -0,0 +1,10 @@
+class MyHotRepository < ActionMailer::Base
+  helper :application
+  default from: Settings.mailer.from
+
+  def notify(user)
+    @user = user
+    @watch_events = @user.watch_events_by_followings.where(created_at: {'$gte' =>  1.day.ago.to_s}).reverse
+    mail(to: user.email, subject: "Watched repositories by #{@user.username}'s followings")
+  end
+end
