20b26ff19d53636395eca7f6030255badfcdffd9
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-24 15:16:00 +0900

    Extract method for watched lanking

--- app/models/watch_event.rb
+++ app/models/watch_event.rb
@@ -2,4 +2,10 @@ class WatchEvent
   include Mongoid::Document
   # TODO enable this
   # default_scope type: 'WatchEvent'
+
+  def self.watched_lanking
+    grouped_events = self.all.group_by {|event| event['repo']['name'] }
+    lanking = grouped_events.sort_by {|repo_name, events| -events.count }
+    lanking
+  end
 end
