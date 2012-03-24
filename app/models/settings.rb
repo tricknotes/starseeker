fdf87d56ddfba8445485cff48680d9d2e89459f5
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-24 16:13:48 +0900

    Load default settings

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,5 @@
 class Settings < Settingslogic
-  source "#{Rails.root}/config/settings.yml"
+  source "#{Rails.root}/config/settings.yml.default"
+  source "#{Rails.root}/config/settings.yml" if File.exist?("#{Rails.root}/config/settings.yml")
   namespace Rails.env
 end
