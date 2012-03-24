c06a5b6edb797a1b109e053bd519aea1c3ed6507
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-24 15:24:33 +0900

    Extract scope for finding newly events

--- app/models/watch_event.rb
+++ app/models/watch_event.rb
@@ -3,6 +3,8 @@ class WatchEvent
   # TODO enable this
   # default_scope type: 'WatchEvent'
 
+  scope :newly, ->(from) { where(created_at: {'$gte' => from}) }
+
   def self.watched_lanking
     grouped_events = self.all.group_by {|event| event['repo']['name'] }
     lanking = grouped_events.sort_by {|repo_name, events| -events.count }
