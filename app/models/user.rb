7431a546b34637b5edbb7891ef6d0f1f3c46309f
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-24 17:11:40 +0900

    Delete accessibility to unused attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :name, :email, :password, :password_confirmation, :authentications_attributes
+  attr_accessible :name, :email, :authentications_attributes
 
   has_many :authentications, :dependent => :destroy
   accepts_nested_attributes_for :authentications
