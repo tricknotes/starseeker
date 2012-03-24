c7fabd7b15c4893d060f7f1e12b0e8ab05dc855f
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-24 16:27:31 +0900

    Fix invalids

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,8 @@ WatchMen::Application.configure do
     :port                 => 587,
     :domain               => 'gmail.com',
     :authentication       => :login,
-    :user_name            => Setting.mail.sender
-    :password             => Setting.mail.password
+    :user_name            => Settings.mail.sender,
+    :password             => Settings.mail.password
   }
 
   # Enable threaded mode
