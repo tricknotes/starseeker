e5a5ebf3b1e2c2c366e18109804ba5e0b47a5b99
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-25 15:51:08 +0900

    Layout code

--- app/models/github_events.rb
+++ app/models/github_events.rb
@@ -1,7 +1,7 @@
 module GithubEvents
   include Typhoeus
-  remote_defaults on_success: lambda {|response| JSON.parse(response.body) },
-                  on_failure: lambda {|response| response.code },
+  remote_defaults on_success: ->(response) { JSON.parse(response.body) },
+                  on_failure: ->(response) { response.code },
                   base_uri: 'https://api.github.com'
 
   define_remote_method :followings, path: '/users/:user/following'
