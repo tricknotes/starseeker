be4d6d425186e006a9a019c0bcfa77c67843e9b2
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-27 22:29:50 +0900

    Show watch and watched someone repositories

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   before_filter :require_login, :assign_curent_user
 
   def show
+    @watch_event = WatchEvent.by(@user.username)
+    @watched_event = WatchEvent.owner(@user.username)
   end
 end
