78318fb5fa375b2063b0ad46a321149a33d0f46d
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-03-27 22:45:58 +0900

    Sort watche event in newly

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_filter :require_login, :assign_curent_user
 
   def show
-    @watch_event = WatchEvent.by(@user.username)
-    @watched_event = WatchEvent.owner(@user.username)
+    @watch_event = WatchEvent.by(@user.username).newly
+    @watched_event = WatchEvent.owner(@user.username).newly
   end
 end
