0841d0fdce32f9d481f6d56a26ecd8691183bdb4
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-01 22:43:07 +0900

    Return empty data if request failed

--- app/models/github_events.rb
+++ app/models/github_events.rb
@@ -1,7 +1,7 @@
 module GithubEvents
   include Typhoeus
   remote_defaults on_success: ->(response) { JSON.parse(response.body) },
-                  on_failure: ->(response) { response.code },
+                  on_failure: ->(response) { [] },
                   base_uri: 'https://api.github.com'
 
   define_remote_method :followings, path: '/users/:user/following'
