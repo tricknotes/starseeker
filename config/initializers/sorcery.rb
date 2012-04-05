tricknotes/starseeker

commit 8c5a7c716eac90c9ad66d96be7ed0f39a207de26

Layout code style

config/initializers/sorcery.rb
@@ -98,10 +98,10 @@ Rails.application.config.sorcery.configure do |config|
   config.github.secret = Settings.github.secret
   config.github.callback_url = "#{Settings.base_url}/oauth/callback?provider=github"
   config.github.user_info_mapping = {
-    :username => 'login',
-    :name => 'name',
-    :email => 'email',
-    :avatar_url => 'avatar_url'
+    username:   'login',
+    name:       'name',
+    semail:     'email',
+    avatar_url: 'avatar_url'
   }
   #
   # config.google.key = ""

commit bf985bc639629849494ec1e210c54fcf6a66014b

Fix typo
User had never been created because of
missing attributes was called!

Broken since 8c5a7c71.
Oops!

config/initializers/sorcery.rb
@@ -100,7 +100,7 @@ Rails.application.config.sorcery.configure do |config|
   config.github.user_info_mapping = {
     username:   'login',
     name:       'name',
-    semail:     'email',
+    email:      'email',
     avatar_url: 'avatar_url'
   }
   #