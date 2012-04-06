94d618f73daf045dcd8876351ea9fb0862465c4b
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-07 00:50:17 +0900

    Add sepc about MyHotRepository

---
+++ spec/mailers/my_hot_repository_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe MyHotRepository do
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe '#notify' do
+    subject { MyHotRepository.notify(user) }
+
+    it 'should contains username' do
+      subject.body.should match('USER')
+    end
+
+    it 'should contains watched reposotories' do
+      pending 'Factories required.'
+    end
+  end
+end
