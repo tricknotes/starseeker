f198c2b62ddb1df53bd1a39e695b5457222a4080
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-07 09:59:14 +0900

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120327120623) do
+ActiveRecord::Schema.define(:version => 20120407005256) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id",    :null => false
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(:version => 20120327120623) do
   create_table "users", :force => true do |t|
     t.string   "username",                                      :null => false
     t.string   "email"
-    t.string   "crypted_password"
-    t.string   "salt"
     t.datetime "created_at",                                    :null => false
     t.datetime "updated_at",                                    :null => false
     t.string   "name"
