261cf83d022ebb50735baf887043758df2a6459e
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-07 23:39:40 +0900

    Use repository name at top of list

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,13 +1,14 @@
 function showRepoDescription() {
-  var $repo_description = $(this);
-  var repo_name = $repo_description.data('repo_name');
+  var $repo = $(this);
+  var repo_name = $repo.data('repo_name');
 
   $.ajax(
       'https://api.github.com/repos/'+repo_name
     , {
         dataType: 'jsonp'
       , success: function(repo) {
-          $repo_description.html(repo.data.description);
+          var $description = $repo.find('.description');
+          $description.html(repo.data.description);
       }
     }
   );
