8c0963779787b9ad36d8416ba44337f0a6b85467
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-08 19:27:00 +0900

    Improve message when user activated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if @user
       @user.activate!
       UserMailer.activation_success_email(@user).deliver
-      redirect_to dashboard_path, notice: 'User was successfully activated.'
+      redirect_to dashboard_path, notice: 'You were successfully activated.'
     else
       not_authenticated
     end
