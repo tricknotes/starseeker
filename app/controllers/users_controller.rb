066445ce138c3d8dde597d942e4fbf1bb7197088
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-14 15:38:43 +0900

    Reduce watch event within 7 days from now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:id])
-    @watch_events = WatchEvent.by(@user.username).newly
+    @watch_events = WatchEvent.by(@user.username).latest(7.days.ago).newly
   end
 end
