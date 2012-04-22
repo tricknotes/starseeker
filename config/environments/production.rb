8cca87a9ad31d07d7f491edc2a091ac1388aa398
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-04-22 21:15:59 +0900

    Ignore error on delivering mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Watchmen::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
