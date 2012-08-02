ba1348b9b4e33fc37f5f07ef1e1317f86a432db4
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-08-03 01:32:31 +0900

    Upgrade rails version to 3.2.6

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Watchmen::Application.config.session_store :cookie_store, key: '_gh-archive_session'
+Watchmen::Application.config.session_store :cookie_store, key: '_watchmen_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
