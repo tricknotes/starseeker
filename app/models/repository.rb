cc5dbae8d6fcf22016b849cb42730e10255ab5cb
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-08-18 21:15:17 +0900

    Modify signiture for `Repository.fetch`

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,8 +2,8 @@ class Repository
   include Mongoid::Document
 
   class << self
-    def fetch(login, name)
-      repo = Octokit.repo(username: login, repo: name)
+    def fetch(reponame)
+      repo = Octokit.repo(reponame)
       new(repo.to_hash)
     end
   end
