448011a88e4fb4a94c4df487951b12302252385d
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-08-20 13:08:08 +0900

    Sort events by watched count and watched time

--- app/models/watch_event.rb
+++ app/models/watch_event.rb
@@ -14,8 +14,8 @@ class WatchEvent
   end
 
   def self.watched_ranking
-    grouped_events = self.all.group_by {|event| event['repo']['name'] }
-    grouped_events = grouped_events.sort_by {|repo_name, events| -events.count }
+    grouped_events = self.all.newly.group_by {|event| event['repo']['name'] }
+    grouped_events = grouped_events.sort_by {|repo_name, events| [-events.count, -events.first.created_at.to_i] }
     grouped_events = grouped_events.map do |repo_name, events|
       repo = events.first.repository!
       [repo_name, events, repo]
