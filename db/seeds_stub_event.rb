d28285f7b31be9361c3023eaf358590687603c57
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-08-21 21:27:56 +0900

    `ENV['GITHUB_LOGIN']` is required

--- db/seeds_stub_event.rb
+++ db/seeds_stub_event.rb
@@ -5,6 +5,9 @@ raise "'production' should not be sutubed." if Rails.env.production?
 WatchEvent.destroy_all
 
 GITHUB_LOGIN = ENV['GITHUB_LOGIN']
+unless GITHUB_LOGIN
+  raise "The environment variable `GITHUB_LOGIN` is required."
+end
 
 def path_to_watch_event(path)
   data = JSON.parse(File.read(path))
@@ -17,11 +20,11 @@ fixture_path = Rails.root.join('spec', 'fixtures', 'watch_events')
 data_path = fixture_path.join('*.json').to_s
 Dir[data_path].each.with_index do |path, n|
   watch_event = path_to_watch_event(path)
-  if GITHUB_LOGIN
-    @user ||= Octokit.user(GITHUB_LOGIN)
-    keys = watch_event.actor.keys
-    watch_event.actor = @user.to_hash.extract!(*keys)
-  end
+
+  @user ||= Octokit.user(GITHUB_LOGIN)
+  keys = watch_event.actor.keys
+  watch_event.actor = @user.to_hash.extract!(*keys)
+
   watch_event.created_at = n.days.ago.strftime(WatchEvent::DATETIME_FORMAT)
   watch_event.save!
 end
@@ -29,15 +32,15 @@ end
 # Setup watch event to `GITHUB_LOGIN`
 Dir[data_path].each.with_index do |path, n|
   watch_event = path_to_watch_event(path)
-  if GITHUB_LOGIN
-    @following ||= Octokit.following(GITHUB_LOGIN)
-    watch_event.actor = @following.sample.extract!(*watch_event.actor.keys)
-    @repos ||= Octokit.repos(GITHUB_LOGIN)
-    repo = @repos.sample
-    watch_event.repo['id']   = repo['id']
-    watch_event.repo['name'] = repo['full_name']
-    watch_event.repo['url']  = repo['url']
-  end
+
+  @following ||= Octokit.following(GITHUB_LOGIN)
+  watch_event.actor = @following.sample.extract!(*watch_event.actor.keys)
+  @repos ||= Octokit.repos(GITHUB_LOGIN)
+  repo = @repos.sample
+  watch_event.repo['id']   = repo['id']
+  watch_event.repo['name'] = repo['full_name']
+  watch_event.repo['url']  = repo['url']
+
   watch_event.created_at = n.hours.ago.strftime(WatchEvent::DATETIME_FORMAT)
   watch_event.save!
 end
