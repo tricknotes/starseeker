26ec7bf43ccfd2d4ae90a553f2275a9c91317df5
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-10-06 12:46:48 +0900

    Add `Settings#host` to extract host from base url

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -2,4 +2,17 @@ class Settings < Settingslogic
   source "#{Rails.root}/config/settings.yml.default"
   source "#{Rails.root}/config/settings.yml" if File.exist?("#{Rails.root}/config/settings.yml")
   namespace Rails.env
+
+  def host
+    if uri.default_port == uri.port
+      uri.host
+    else
+      '%s:%d' % [uri.host, uri.port]
+    end
+  end
+
+  private
+  def uri
+    @uri ||= URI.parse(self.base_url)
+  end
 end
