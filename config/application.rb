1d95dbd9df0c8c8fe8c15ff64f83df38882f01b4
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-01-08 23:30:57 +0900

    Add require for mongoid

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require 'mongoid/railtie'
+
 # Assets should be precompiled for production (so we don't need the gems loaded then)
 Bundler.require(*Rails.groups(assets: %w(development test)))
 
