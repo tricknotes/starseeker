766148748a803914aa998a0d7e0ee22094d7d72f
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-01-11 18:17:23 +0900

    Add `attr_accessible` for signup

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,6 @@
 class Authentication < ActiveRecord::Base
+  # TODO I wish to eliminate `attr_accessible`!
+  # patch for sorcery with rails4.
+  attr_accessible :user_id, :provider, :uid
   belongs_to :user
 end
