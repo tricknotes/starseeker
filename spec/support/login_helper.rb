dc286c221481ee9498e4d5e24c4567e9603c936b
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-01-18 18:00:19 +0900

    Extract module about login helper

---
+++ spec/support/login_helper.rb
@@ -0,0 +1,31 @@
+module LoginHelper
+  def stub_login!(user)
+    # Stub '#login_at' in '/oauth/:provider'
+    original_login_at = OauthsController.instance_method(:login_at)
+    OauthsController.send(:define_method, :login_at) do |provider|
+
+      redirect_to action: :callback, provider: provider
+
+      # Remove stub
+      OauthsController.send(:define_method, :login_at) do |provider|
+        original_login_at.bind(self).call(provider)
+      end
+    end
+
+    # Stub `#login_from` in '/oauth/callback'
+    original_login_from = OauthsController.instance_method(:login_from)
+    OauthsController.send(:define_method, :login_from) do |provider|
+      auto_login(user)
+      after_login!(user)
+
+      # Remove stub
+      OauthsController.send(:define_method, :login_from) do |provider|
+        original_login_from.bind(self).call(provider)
+      end
+
+      user
+    end
+  end
+end
+
+RSpec.configuration.include LoginHelper, capybara_feature: true
--- spec/support/stub_helper.rb
+++ spec/support/stub_helper.rb
@@ -17,34 +17,6 @@ module StubHelper
       .to_return(body: repo)
     Repository.fetch!(name)
   end
-
-  def stub_login!(user)
-    # Stub '#login_at' in '/oauth/:provider'
-    original_login_at = OauthsController.instance_method(:login_at)
-    OauthsController.send(:define_method, :login_at) do |provider|
-
-      redirect_to action: :callback, provider: provider
-
-      # Remove stub
-      OauthsController.send(:define_method, :login_at) do |provider|
-        original_login_at.bind(self).call(provider)
-      end
-    end
-
-    # Stub `#login_from` in '/oauth/callback'
-    original_login_from = OauthsController.instance_method(:login_from)
-    OauthsController.send(:define_method, :login_from) do |provider|
-      auto_login(user)
-      after_login!(user)
-
-      # Remove stub
-      OauthsController.send(:define_method, :login_from) do |provider|
-        original_login_from.bind(self).call(provider)
-      end
-
-      user
-    end
-  end
 end
 
 RSpec.configuration.include StubHelper, capybara_feature: true
