969e76b4dd331211c1d4128cb65b59bfe70d1431
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-01-18 18:45:27 +0900

    Avoid save failure for duplicted id

--- spec/support/stub_helper.rb
+++ spec/support/stub_helper.rb
@@ -2,20 +2,30 @@ module StubHelper
 
   FIXTURE_PATH = Rails.root.join('spec', 'fixtures')
 
+  def unique_id
+    @unique_id ||= rand(10000)
+    @unique_id += 1
+  end
+
   def stub_star_event!(attrs)
     json = FIXTURE_PATH.join('watch_events', '1.json').read
     event = JSON.parse(json)
-    event = event.merge('created_at' => DateTime.now.strftime(StarEvent::DATETIME_FORMAT))
+    event = event.merge(
+      'id' => unique_id,
+      'created_at' => DateTime.now.strftime(StarEvent::DATETIME_FORMAT)
+    )
     StarEvent.create!(event.merge(attrs))
   end
 
   def stub_repository!(name, attrs = {})
     json = FIXTURE_PATH.join('repositories', '1.json').read
     repo = JSON.parse(json)
-    repo = repo.merge(attrs).merge('full_name' => name)
-    stub_request(:get, URI.join(Octokit.api_endpoint, 'repos/' + name).to_s)
-      .to_return(body: repo)
-    Repository.fetch!(name)
+    repo = repo.merge(
+      'id' => unique_id,
+      'full_name' => name
+    )
+    repo = repo.merge(attrs)
+    Repository.create!(repo)
   end
 end
 
