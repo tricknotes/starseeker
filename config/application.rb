da7a801bf236702abd2eb5fc521621c107da4f34
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-01-23 20:00:01 +0900

    Update bundle with cofiguration regeneration

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,12 @@ module Starseeker
     # Enable the asset pipeline.
     config.assets.enabled = true
 
-    # Version of your assets, change this if you want to expire all your assets.
-    config.assets.version = '1.0'
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
   end
 end
