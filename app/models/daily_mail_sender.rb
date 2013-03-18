d89e73d287cbcaf8496b53e6870fa101a04e8742
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-03-19 02:25:25 +0900

    Schedule sending hot repository mail using redis

---
+++ app/models/daily_mail_sender.rb
@@ -0,0 +1,90 @@
+module DailyMailSender
+  TABLE_NAME = 'users_to_be_sent_email'
+
+  module Status
+    SCHEDULED   = 'scheduled'
+    FAILED      = 'failed'
+    PROCESSIONG = 'processing'
+  end
+
+  class << self
+    attr_accessor :logger
+
+    def redis
+      @redis ||= begin
+        uri = URI.parse(Settings.redis.url)
+        Redis.new(host: uri.host, port: uri.port, password: uri.password)
+      end
+    end
+
+    def schedule(users)
+      statuses = users.flat_map {|user| [user.id, Status::SCHEDULED] }
+
+      redis.hmset TABLE_NAME, *statuses
+    end
+
+    def send_mail_to_scheduled_users
+      scheduled_users.each do |user|
+        # TODO Enable transaction
+        case status_for_user(user)
+        when Status::SCHEDULED, Status::FAILED
+          start_processing(user)
+
+          label = '%s(%s)' % [user.username, user.email]
+
+          begin
+            if user_has_starred?(user, 1.day.ago)
+              MyHotRepository.notify(user).deliver
+
+              self.logger.info "Send hot repositories mail to \033[36m%s\033[39m." % [label]
+            else
+              self.logger.info "Skip sending mail to \033[33m%s\033[39m. Because star events to him are empty." % [label]
+            end
+
+            finish_sending_mail user
+          rescue Octokit::Unauthorized
+            finish_sending_mail user
+
+            self.logger.info "Skip sending mail to \033[31m%s\033[39m. Because of unauthorized Token." % [label]
+          rescue => e
+            schedule_as_retry user
+
+            self.logger.error ["#{e.class} #{e.message}:", *e.backtrace.map {|m| '  '+m }].join("\n")
+          end
+        else
+          # noop
+        end
+      end
+    end
+
+    private
+
+    def scheduled_users
+      user_ids = redis.hkeys(TABLE_NAME)
+      User.find(user_ids)
+    end
+
+    def status_for_user(user)
+      redis.hmget(TABLE_NAME, user.id).first
+    end
+
+    def start_processing(user)
+      redis.hmset(TABLE_NAME, user.id, Status::PROCESSIONG)
+    end
+
+    def finish_sending_mail(user)
+      redis.hdel(TABLE_NAME, user.id)
+    end
+
+    def schedule_as_retry(user)
+      redis.hmset(TABLE_NAME, user.id, Status::FAILED)
+    end
+
+    def user_has_starred?(user, term)
+      user.star_events_by_followings_with_me.latest(term).present?
+    end
+  end
+
+  self.logger = Rails.logger
+
+end
--- lib/tasks/send_hot_repositories.rake
+++ lib/tasks/send_hot_repositories.rake
@@ -2,21 +2,5 @@ require "#{Rails.root}/config/environment"
 
 desc 'Send hot repositories mail to all users'
 task :send_hot_repositories do
-  User.email_sendables.each do |user|
-    label = '%s(%s)' % [user.username, user.email]
-
-    begin
-      if user.star_events_by_followings_with_me.latest(1.day.ago).present?
-        MyHotRepository.notify(user).deliver
-
-        message = "Send hot repositories mail to \033[36m%s\033[39m." % [label]
-      else
-        message = "Skip sending mail to \033[33m%s\033[39m. Because star events to him are empty." % [label]
-      end
-    rescue Octokit::Unauthorized
-      message = "Skip sending mail to \033[31m%s\033[39m. Because of unauthorized Token." % [label]
-    end
-
-    puts message
-  end
+  DailyMailSender.send_mail_to_scheduled_users
 end
