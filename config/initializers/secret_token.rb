2b1fa393cb38173a6d9cad423e441919c475b3b8
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-04-11 00:08:29 +0900

    Use secret token from ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Starseeker::Application.config.secret_key_base = '9655c5fd9112b357c534c062a8998992e3c63b186f12062a383d19a95ad9d7c104eee48971642772c98b0353bcb3e4edb8279ee60ddc005b39765c567c7a6676'
+Starseeker::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || '9655c5fd9112b357c534c062a8998992e3c63b186f12062a383d19a95ad9d7c104eee48971642772c98b0353bcb3e4edb8279ee60ddc005b39765c567c7a6676'
