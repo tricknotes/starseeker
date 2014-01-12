01233cec744d0aac23a0a0614404e1ad56ac4536
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-01-12 15:55:52 +0900

    Remove unused helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def gravatar_url(gravatar_id)
-    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png"
-  end
-
   def github_url(*name)
     "https://github.com/#{name.join('/')}"
   end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe '#gravatar_url' do
-    it 'should cantains gravatar_id' do
-      gravatar_url('TEST').should match(%r{https://secure.gravatar.com/avatar/TEST})
-    end
-  end
-
   describe '#github_url' do
     it 'should contains name' do
       github_url('ACTOR').should eq('https://github.com/ACTOR')
