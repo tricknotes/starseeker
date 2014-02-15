8271a2d9e2ff2e8e0a922c4223ec9c056cf886fe
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-02-16 01:09:39 +0900

    Fix link to GitHub language
    This issue was reported by @hrysd .
    Cheers :soy_milk:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
-  def github_url(*name)
-    "https://github.com/#{name.join('/')}"
+  def github_url(path = '/', query = nil)
+    path = '/' + path unless path[0] == '/'
+
+    uri = URI.parse('https://github.com')
+    uri.path = path
+    uri.query = query.to_query if query
+    uri.to_s
   end
 
   def link_to_repo(repo_name)
@@ -10,11 +15,11 @@ module ApplicationHelper
   def link_to_language(repo)
     return '' unless repo.language
 
-    link_to "##{repo.language}", github_url('languages', repo.language)
+    link_to "##{repo.language}", github_url('trending', l: repo.language)
   end
 
   def link_to_stargazers(repo)
-    link_to('[%d]' % repo.stargazers_count, github_url(repo.full_name, 'stargazers'))
+    link_to('[%d]' % repo.stargazers_count, github_url("#{repo.full_name}/stargazers"))
   end
 
   def image_link_to_github_url(user, size = '30x30')
