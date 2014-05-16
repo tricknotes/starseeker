8b4efb229129d756ba4647f39dc029135fdb2da8
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-05-17 04:37:46 +0900

    Move font file to correct path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true # For web fonts
+  config.serve_static_assets = true # for Heroku
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
