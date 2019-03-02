79df02331896542c1d939cb88cdd65db35d930e1
Ryunosuke Sato <tricknotes.rs@gmail.com>
2019-03-03 01:58:14 +0900

    Fix to use block syntax for factory definition
    The static attributes are removed in factory_bot 5.
    Ref: https://github.com/thoughtbot/factory_bot/pull/1163

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    username 'USER'
-    name 'starseeker user'
+    username { 'USER' }
+    name { 'starseeker user' }
     sequence(:email) {|i| "user-#{i}@starseeker.so" }
-    subscribe true
-    activation_state 'active'
-    avatar_url 'http://example.com/avater.png'
+    subscribe { true }
+    activation_state { 'active' }
+    avatar_url { 'http://example.com/avater.png' }
 
     trait :with_authentication do
       after(:create) do |user, _evaluator|
@@ -15,8 +15,8 @@ FactoryBot.define do
   end
 
   factory :github, class: :authentication do
-    provider 'github'
-    uid 'GITHUB'
-    token 'GITHUB_TOKEN'
+    provider { 'github' }
+    uid { 'GITHUB' }
+    token { 'GITHUB_TOKEN' }
   end
 end
