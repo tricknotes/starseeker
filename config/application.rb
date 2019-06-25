bcfe3c09359bd3dd18cf06451e65d33f10cb381b
Ryunosuke Sato <tricknotes.rs@gmail.com>
2019-06-25 20:58:39 +0900

    Update config to Rails 6.0

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ Bundler.require(*Rails.groups)
 module Starseeker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
+    config.autoloader = :classic # TODO Use zeitwerk
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
