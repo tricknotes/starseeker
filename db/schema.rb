1828f0d00daff11fd4b2398648979e8748363655
Ryunosuke Sato <tricknotes.rs@gmail.com>
2019-06-25 19:58:43 +0900

    $ bin/update

--- db/schema.rb
+++ db/schema.rb
@@ -2,39 +2,40 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2012_12_18_110908) do
+ActiveRecord::Schema.define(version: 2014_09_21_032348) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.string "provider", null: false
-    t.string "uid", null: false
+    t.string "provider", limit: 255, null: false
+    t.string "uid", limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "token"
+    t.string "token", limit: 255
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "email"
+    t.string "username", limit: 255, null: false
+    t.string "email", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "name"
-    t.string "avatar_url"
+    t.string "name", limit: 255
+    t.string "avatar_url", limit: 255
     t.boolean "subscribe", default: true
-    t.string "activation_state"
-    t.string "activation_token"
+    t.string "activation_state", limit: 255
+    t.string "activation_token", limit: 255
     t.datetime "activation_token_expires_at"
-    t.string "crypted_password"
-    t.string "salt"
-    t.string "feed_token"
+    t.string "crypted_password", limit: 255
+    t.string "salt", limit: 255
+    t.string "feed_token", limit: 255
+    t.integer "daily_mail_hour", default: 9, null: false
     t.index ["activation_token"], name: "index_users_on_activation_token"
   end
 
