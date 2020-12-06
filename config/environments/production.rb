b62147055dcd26079e700245cd6f9e1c633745c3
Ryunosuke Sato <tricknotes.rs@gmail.com>
2020-12-06 11:50:17 +0900

    Use `before_initialize` to reference `Settings` that is defined under `app/models`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = Hash[Settings.mail].symbolize_keys
+  # To reference `Settings`, `before_initialize` is required.
+  config.before_initialize do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.smtp_settings = Hash[Settings.mail].symbolize_keys
+  end
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
