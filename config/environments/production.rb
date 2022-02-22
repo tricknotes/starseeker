a3658876828634b527b3da480b6283f4950ae9ce
Ryunosuke Sato <tricknotes.rs@gmail.com>
2022-02-22 22:45:58 +0900

    Specify mail setting directly
    To avoid a settingslogic dependency.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,12 +60,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
-  # To reference `Settings`, `before_initialize` is required.
-  config.before_initialize do
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.smtp_settings = Hash[Settings.mail].symbolize_keys
-  end
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'heroku.com',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
