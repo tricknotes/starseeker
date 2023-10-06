7957a31ee00fdb58eb38e0f1d3f65134aa7fd85f
Ryunosuke Sato <tricknotes.rs@gmail.com>
2023-10-06 23:05:23 +0900

    `bin/rails app:update` with some changes

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module Starseeker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -31,6 +36,9 @@ module Starseeker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
 
     config.after_initialize do
       Rails.application.default_url_options = Settings.url_options
